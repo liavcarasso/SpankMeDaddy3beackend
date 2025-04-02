5d1725b92e5b5aa692abf1c71147f852cfaa06e8
liavcarasso <126164188+liavcarasso@users.noreply.github.com>
2025-04-02 16:18:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
 import sqlite3
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
